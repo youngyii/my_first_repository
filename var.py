2ef742cfe6d36046025d3167be0a251d6d156bd3
youngyii <yishinyoung0708@gmail.com>
2023-09-10 22:10:51 +0900

    add var

--- var.py
+++ var.py
@@ -118,7 +118,7 @@ print(my_list)
 del my_list[2]
 print(*my_list)
 '''
-
+'''
 my_tuple = (1, 2, 'three', True)
 print(my_tuple)
 tpItem = (3.14, "b", 'two', False)
@@ -154,3 +154,59 @@ print(my_tup.index(2, 0, 3))
 print(my_tup.index("py", 3, 5))
 
 #print(my_tup.index(1, 0, 3)) #에러
+'''
+'''
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+print(my_dict)
+print(*my_dict)
+dicData = {"name" : "python", "number" : 23564897}
+print(dicData)
+
+my_item = my_dict.items()
+print(my_item)
+
+idx = ("key1", "key2", "key3")
+dicts = dict.fromkeys(idx, "0")
+print(dicts)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+print(my_dict["key1"])
+
+my_str = my_dict.get("key2")
+print(my_str)
+
+print(my_dict.pop("key1"))
+print(my_dict)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicData = {"name" : "python", "number" : 23564897}
+my_dict["key3"] = "value3"
+print(my_dict)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+my_dict.setdefault("key3")
+print(my_dict)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+my_dict.update({"key1": "v4"})
+print(my_dict)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+del my_dict["key2"]
+print(my_dict)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+print("key2" in my_dict)
+print("key3" in my_dict)
+
+my_list = list(my_dict.keys())
+print(my_list)
+my_list = list(my_dict.values())
+print(my_list)
+
+my_set = set(my_dict.keys())
+print(my_set)
+
+my_dict.clear()
+print(my_dict)
+'''
